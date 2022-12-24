0183b79ecb32588d11f23cf1903479d0d33c0a2e
OleksandrNovyk <novykav@gmail.com>
2022-12-24 16:11:18 +0200

    Summary

---
+++ src/index.js
@@ -0,0 +1,15 @@
+
+new Swiper('.slider', {
+
+    navigation: {
+        nextEl: 'swiper-button-next',
+        prevEl: 'swiper-button-prev'
+    },
+    
+    
+    pagination:{
+        el:'.swiper-pagination',
+        clicable: true,
+    },
+});
+
