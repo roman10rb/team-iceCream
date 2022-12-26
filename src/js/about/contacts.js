8b93fe6c55003297e8c5cde64f02aa4853e75246
KolomiAnna <115336722+KolomiAnna@users.noreply.github.com>
2022-12-26 19:48:24 +0200

    Modal

--- src/js/about/contacts.js
+++ src/js/about/contacts.js
@@ -1,18 +1,22 @@
-(() => {
-    const refs = {
-      openModalBtn: document.querySelector("[data-modal-open5]"),
-      openModalBtn1: document.querySelector("[data-modal-open6]"),
 
-      closeModalBtn: document.querySelector("[data-modal-close5]"),
-      modal: document.querySelector("[data-modal5]"),
-    };
-  
-    refs.openModalBtn.addEventListener("click", toggleModal);
-    refs.openModalBtn1.addEventListener("click", toggleModal);
-    refs.openModalBtn2.addEventListener("click", toggleModal);
-    refs.closeModalBtn.addEventListener("click", toggleModal);
-  
-    function toggleModal() {
-      refs.modal.classList.toggle("is-hidden");
-    }
-  })();
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector("[data-modal-open5]"),
+    openModalBtn1: document.querySelector("[data-modal-open6]"),
+
+    closeModalBtn: document.querySelector("[data-modal-close5]"),
+    closeModalBtn: document.querySelector("[data-modal-close6]"),
+    modal: document.querySelector("[data-modal5]"),
+    modal: document.querySelector("[data-modal6]"),
+
+  };
+
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.openModalBtn1.addEventListener("click", toggleModal);
+  refs.openModalBtn2.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle("is-hidden");
+  }
+})();
