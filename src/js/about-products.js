0b17c3b00783415cc840f44ff61da5765c62c9fd
VladislavKondakov <agentvl@ukr.net>
2022-12-26 14:04:35 +0100

    opened with 3 buttons

--- src/js/about-products.js
+++ src/js/about-products.js
@@ -1,11 +1,15 @@
 (() => {
   const refs = {
     openModalBtn: document.querySelector("[data-modal-open1]"),
+    openModalBtn1: document.querySelector("[data-modal-open2]"),
+    openModalBtn2: document.querySelector("[data-modal-open3]"),
     closeModalBtn: document.querySelector("[data-modal-close1]"),
     modal: document.querySelector("[data-modal1]"),
   };
 
   refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.openModalBtn1.addEventListener("click", toggleModal);
+  refs.openModalBtn2.addEventListener("click", toggleModal);
   refs.closeModalBtn.addEventListener("click", toggleModal);
 
   function toggleModal() {
