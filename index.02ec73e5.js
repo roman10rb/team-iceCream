4e53f86b1a36be9116259a04b6e87390ac737293
roman10rb <114180387+roman10rb@users.noreply.github.com>
2022-12-27 12:26:20 +0000

    Deploying to gh-pages from @ roman10rb/team-iceCream@5fa508618a2f97cee6449c4388a7907af113e138 🚀

---
+++ index.02ec73e5.js
@@ -0,0 +1,2 @@
+!function(){var e={openModalBtn:document.querySelector("[data-modal2-open]"),closeModalBtn:document.querySelector("[data-modal2-close]"),modal:document.querySelector("[data-modal2]")};function o(){e.modal.classList.toggle("is-hidden2")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)}();
+//# sourceMappingURL=index.02ec73e5.js.map
---
+++ index.b7130e04.js
@@ -0,0 +1,2 @@
+!function(){var e={openModalBtn:document.querySelector("[data-modal-m-menu-open]"),closeModalBtn:document.querySelector("[data-modal-m-menu-close]"),modal:document.querySelector("[data-modal-m-menu]")};function o(){e.modal.classList.toggle("is-hidden-m-menu")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)}();
+//# sourceMappingURL=index.b7130e04.js.map
